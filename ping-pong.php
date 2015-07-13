48a2e417e2dfe16bba948d08a1b2b7a2ac0c95ad
Fabien Udriot <fabarea@users.noreply.github.com>
2015-07-13 17:43:50 +0200

    Update ping-pong.php

--- ping-pong.php
+++ ping-pong.php
@@ -3,16 +3,38 @@
 /*
 Script CLI (command line interface) for synchronizing two database
 */
-include('Classes/Database.php');
-include('Classes/Logger.php');
-$logger = new Logger();
+#@todo
 
-$dbOld = new Ecodev\Database('localhost', 'root', 'root');
-$dbOld->connect('DB-old');
-$dbNew = new Ecodev\Database('localhost', 'root', 'root');
-$dbNew->connect('DB-new');
+// Tables to be created into the new database
+$importTables = array(
+	'tx_dam',
+	'tx_dam_cat',
+	'tx_dam_mm_cat',
+	'tx_datafilter_filters',
+	'tx_dataquery_queries',
+	'tx_displaycontroller_components_mm',
+	'tx_phpdisplay_displays',
+	'tx_speciality_domain_model_domain',
+	'tx_speciality_domain_model_group',
+	'tx_speciality_domain_model_master',
+	'tx_templatedisplay_displays'
+);
 
-$convertFields = array(
+// Tables to be synchronized into the new database.
+$synchronizeTables = array(
+	'tt_content',
+	'pages',
+	'be_groups',
+	'be_users',
+	'fe_groups',
+	'fe_users',
+	'sys_domain',
+	'sys_refindex',
+	'sys_template',
+);
+
+// Special rules applied against synchronized tables.
+$synchronizeRuleTables = array(
 	'tt_content' => array(
 
 		'subheader',
@@ -36,17 +58,18 @@ $convertFields = array(
 	'sys_template' => array(),
 );
 
-$tables = array(
-	'tt_content',
-	'pages',
-	'be_groups',
-	'be_users',
-	'fe_groups',
-	'fe_users',
-	'sys_domain',
-	'sys_refindex',
-	'sys_template',
-);
+############################################
+
+include('Classes/Database.php');
+include('Classes/Logger.php');
+$logger = new Logger();
+
+$dbOld = new Ecodev\Database('localhost', 'root', 'root');
+$dbOld->connect('DB-old');
+$dbNew = new Ecodev\Database('localhost', 'root', 'root');
+$dbNew->connect('DB-new');
+
+
 foreach ($tables as $table) {
 	$logger->log('Synchronizing table " ' . $table . ' " .....');
 	$fieldStructures = $dbNew->select('SHOW COLUMNS FROM ' . $table);
@@ -54,7 +77,29 @@ foreach ($tables as $table) {
 	foreach ($fieldStructures as $fieldStructure) {
 		$newFieldsNames[] = $fieldStructure['Field'];
 	}
-	$toImportValues = $dbOld->select('SELECT * FROM ' . $table);
+	
+	# @todo ne pas synchroniser les records qui ont:
+	# le champ s'appelle "deleted", s'il existe  --> le flag qui marque que le record est suppriméer
+	# le champ peut s'appeler disable ou hidden, s'il existe --> le flag qui marque que le record est masqué
+	
+	// première manière d'implémenter:
+	$clause = '1 = 1';
+	if (isset($newFieldsNames['deleted'])) {
+		$clause .= ' deleted = 1'	
+	}
+	if (isset($newFieldsNames['disable'])) {
+		$clause .= ' disable = 1'	
+	}
+	
+	// deuxième manière d'implémenter:
+	$specialFields = array('deleted', 'disable', 'hidden');
+	foreach ($specialFields as $specialField) {
+		if (isset($newFieldsNames['disable'])) {
+			$clause .= ' disable = 1'	
+		}	
+	}
+	// same for hidden
+	$toImportValues = $dbOld->select('SELECT * FROM ' . $table . ' WHERE ' . $clause);
 	$dbNew->delete($table);//truncating table
 	/*
 	$dbNew->query('TRUNCATE TABLE '. $table); => other way to do it
@@ -74,21 +119,12 @@ foreach ($tables as $table) {
 	}
 	$logger->log('Synchronized!!!');
 }
-$logger->log('
-All tables synchronized succesfully!!!');
-$tables = array(
-	'tx_dam',
-	'tx_dam_cat',
-	'tx_dam_mm_cat',
-	'tx_datafilter_filters',
-	'tx_dataquery_queries',
-	'tx_displaycontroller_components_mm',
-	'tx_phpdisplay_displays',
-	'tx_speciality_domain_model_domain',
-	'tx_speciality_domain_model_group',
-	'tx_speciality_domain_model_master',
-	'tx_templatedisplay_displays'
-);
+# @todo
+$logger->log();
+$logger->log('All tables synchronized succesfully!!!');
+
+# @todo mettre cette variable au sommet
+
 foreach ($tables as $table) {
 	$logger->log('Import whole content of table: "' . $table.'"');
 	$exportCommand = 'mysqldump -u root -p"root" DB-old ' . $table . ' > /tmp/' . $table . '.sql';
@@ -97,5 +133,6 @@ foreach ($tables as $table) {
 	exec($importCommand);
 	$logger->log('Done for:' . $table . '!!!');
 }
-$logger->log('
-All tables imported succesfully in the new database!!!');
+# @todo 
+$loger->log();
+$logger->log('All tables imported succesfully in the new database!!!');
